646a9d2ed90c12215fe1f7c141248da59effce8a
Deshan Bandara <126126025+DeshanBandara@users.noreply.github.com>
2024-03-18 07:41:55 +0530

    added create user

--- frontend/src/Component/CreateUser.jsx
+++ frontend/src/Component/CreateUser.jsx
@@ -1,24 +1,41 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreateUser = () => {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [age, setAge] = useState()
+    const navigate = useNavigate ()
+
+    const Submit = (e) => {
+        e.preventDefault()
+        axios.post(`http://localhost:3002/createUser`, {name, email, age})
+        .then(result => {
+            console.log(result)
+            navigate('/')
+        })
+        .catch(err => console.log(err))
+    }
+
   return (
     <div id='create'>
         <div className='containerCreate'>
-            <form>
+            <form onSubmit={Submit}>
                 <h2>Add user</h2>
                 <div className='addD'>
                     <label htmlFor='name'>Name</label>
-                    <input type='text' placeholder='Enter name' className='formControl' />
+                    <input type='text' placeholder='Enter name' className='formControl' onChange={(e) => setName(e.target.value)}/>
                 </div>
                 <div className='addD'>
                     <label htmlFor='email'>Email</label>
-                    <input type='text' placeholder='Enter name' className='formControl' />
+                    <input type='text' placeholder='Enter name' className='formControl' onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className='addD'>
                     <label htmlFor='age'>Age</label>
-                    <input type='text' placeholder='Enter name' className='formControl' />
+                    <input type='text' placeholder='Enter name' className='formControl' onChange={(e) => setAge(e.target.value)}/>
                 </div>
                 <button className='submitBtn'>Submit</button>
             </form>
